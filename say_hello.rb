39a8bceb1c1741a596547ace57c3ac0e76392a22
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-10 02:56:36 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,3 @@
-def say_hello(gabriela="Ruby programmer", language="Ruby")
+def say_hello(name="Ruby programmer", language="Ruby")
   puts "Hello, #{name}. We heard you are a great #{language} programmer."
 end
